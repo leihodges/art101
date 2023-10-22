79204d96e5b90f22509e43602762733955f35a25
leihodges <leilah4114@gmail.com>
2023-10-21 20:14:54 -0700

    errors in lab 4 changed

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,17 @@
 // Hangman
-// An implemation of the game
-// Leilah Hodges and Maile Jones
-// 19 October, 2023
+// How to play the computer game, Hangman
+// Leilah Hodges <lhodges@ucsc.edu> Maile Jones <matajone@ucsc.edu>
+// 18 October, 2023
+
 
 // Print the base picture
-// Generate dashes for the word
-// Validate user guesses 
-// Evaluate guesses
-    // Test of correct or incorrect letter input
-// Test of win or draw until dashes or picture is complete
+	// Print two horizontal line and one vertical line connected together
+// Have computer select a word larger than 4 letters from dictionary
+	// Generate empthy dashes to replace the letters of the word chosen
+// Users guess one letter at a time 
+	// Validate user guesses
+	// Evaluate guesses
+		// Test if user guess is correct
+			// print part of stick figure if incorrect (one circle, one vertical line, 2 forward slashes, 2 back slashes)
+// Test for win or draw until the stick figure is complete
 
