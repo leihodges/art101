c60e08c7925228b4743c667c6c866adf6297ad49
leihodges <leilah4114@gmail.com>
2023-10-25 10:52:21 -0700

    creation and edits to lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Authors: Leilah Hodges and Phoebe Rettberg
+// Date: 24 October, 2023
 
-// Constants
+// Define Variables
+make="Ford";
+model="Fusion";
+color="white";
+year=2009;
 
-// Functions
+// calculate
+age= 2023-year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:"+ model +"<br>")
+document.writeln("Color:" + color +"<br>")
+document.writeln("Year:" + year +"<br>")
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
